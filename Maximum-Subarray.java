3c7b8e11493382817d4c9e155eb8cc04b8c9cbe9
Abhishek Choudhary <abhishek.choudhary2242@gmail.com>
2024-03-09 20:54:08 +0530

    #

---
+++ Maximum-Subarray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int res = nums[0];
+        for(int i=1;i<nums.length;i++){
+            sum = Math.max(sum+nums[i],nums[i]);
+            res = Math.max(sum,res);
+        }
+        return res;
+    }
+}
